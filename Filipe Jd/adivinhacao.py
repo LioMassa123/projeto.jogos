e379ca3f58e4424c6b4e830f86b9b4046f4d69ed
LioMassa123 <163156996+LioMassa123@users.noreply.github.com>
2024-03-12 12:18:08 -0300

    Add files via upload

--- Filipe Jd/adivinhacao.py
+++ Filipe Jd/adivinhacao.py
@@ -4,16 +4,16 @@ print("########## seja bem vindo ao JOGO DE ADIVINHAÇÃO!!!!!!")
 dificuldade =int(input("selecione a dificuldade: 1-fácil (7 tentativas); 2-médio (5 tentativas), 3-difícil (3 tentivas); 4-impossível! (1 tentativa) \n"))
 
 if (dificuldade == 1):
-    tentativa = 7
+    max_tentativa = 7
 
 elif (dificuldade == 2):
-    tentativa = 5
+    max_tentativa = 5
 
 elif (dificuldade == 3):
-    tentativa = 3
+    max_tentativa = 3
 
 elif (dificuldade == 4):
-    tentativa = 1
+    max_tentativa = 1
 
 else :
     print("dificuldade inválida!")
@@ -23,8 +23,8 @@ sorteio = random.randint(0,10)
 
 
 
-for i in range(tentativa) :
-    chute = int(input(f"você tem {tentativa} tentativa(s) - faça seu chute!, entre 0 a 10: \n"))
+for i in range(max_tentativa) :
+    chute = int(input(f"você tem {max_tentativa} tentativa(s) - faça seu chute!, entre 0 a 10: \n"))
     acertou = chute == sorteio
     menor = chute < sorteio
     maior = chute > sorteio
@@ -38,7 +38,6 @@ for i in range(tentativa) :
     elif(maior):
         print("o número sorteado é menor!")
     
-    tentativa = tentativa-1
 
 if(acertou == 0):
     print("####### O Jogo acabou :( o número era: ", sorteio)    
