e385fbf46903d539ef4e455210d4146313020a0d
LioMassa123 <163156996+LioMassa123@users.noreply.github.com>
2024-03-18 12:20:03 -0300

    Add files via upload

--- Filipe Jd/forca.py
+++ Filipe Jd/forca.py
@@ -1,6 +1,33 @@
+import random
+
 def jogar():
     print("#### Bem Vindo ao jogo de Forca!!!!!")
 
+    palavraN = random.randint(1,6)
+
+    if(palavraN == 1):
+        palavra: "estrela"
+
+    elif(palavraN == 2):
+        palavra: "tigre"
+
+    elif(palavraN == 3):
+        palavra: "escola"
+    
+    elif(palavraN == 4):
+        palavra: "magico"
+    
+    elif(palavraN == 5):
+        palavra: "dragao"
+
+    elif(palavraN == 6):
+        palavra: "fogo"
+
+    acertou= False
+    inforcou= False
+    while(not acertou and not inforcou):
+        print("jogando...")
+        
 
     print("##### Fim do Jogo :(")
 
